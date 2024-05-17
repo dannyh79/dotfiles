08fecd20cd9bd9f9d0ec63e665e6d48af9aeb50e
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 13:49:29 +0800

    Move lspkind-nvim config to plugins/

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -63,56 +63,7 @@ return {
       }
     end
   },
-  "nvim-lua/plenary.nvim",  -- Common utilities
-  {
-    "onsails/lspkind-nvim", -- vscode-like pictograms
-    config = function()
-      require("lspkind").init({
-        -- enables text annotations
-        --
-        -- default: true
-        mode = 'symbol',
-
-        -- default symbol map
-        -- can be either 'default' (requires nerd-fonts font) or
-        -- 'codicons' for codicon preset (requires vscode-codicons font)
-        --
-        -- default: 'default'
-        preset = 'codicons',
-
-        -- override preset symbols
-        --
-        -- default: {}
-        symbol_map = {
-          Text = "",
-          Method = "",
-          Function = "",
-          Constructor = "",
-          Field = "ﰠ",
-          Variable = "",
-          Class = "ﴯ",
-          Interface = "",
-          Module = "",
-          Property = "ﰠ",
-          Unit = "塞",
-          Value = "",
-          Enum = "",
-          Keyword = "",
-          Snippet = "",
-          Color = "",
-          File = "",
-          Reference = "",
-          Folder = "",
-          EnumMember = "",
-          Constant = "",
-          Struct = "פּ",
-          Event = "",
-          Operator = "",
-          TypeParameter = ""
-        },
-      })
-    end
-  },
+  "nvim-lua/plenary.nvim", -- Common utilities
 
   -- purescript 2024-03-07 13:36:46 +0800
   -- Syntax highlighting
