48e573af1eefedac3a0009e8ba0e383cfa8853fe
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 13:34:22 +0800

    Move nvim-bufferline config to plugins/

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -652,44 +652,6 @@ return {
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
-  {
-    "akinsho/nvim-bufferline.lua",
-    config = function()
-      require("bufferline").setup({
-        options = {
-          mode = "tabs",
-          separator_style = 'slant',
-          always_show_bufferline = false,
-          show_buffer_close_icons = false,
-          show_close_icon = false,
-          color_icons = true
-        },
-        --  highlights = {
-        --    separator = {
-        --      fg = '#073642',
-        --      bg = '#002b36',
-        --    },
-        --    separator_selected = {
-        --      fg = '#073642',
-        --    },
-        --    background = {
-        --      fg = '#657b83',
-        --      bg = '#002b36'
-        --    },
-        --    buffer_selected = {
-        --      fg = '#fdf6e3',
-        --      bold = true,
-        --    },
-        --    fill = {
-        --      bg = '#073642'
-        --    }
-        --  },
-      })
-
-      vim.keymap.set('n', '<C-m>', '<Cmd>BufferLineCycleNext<CR>', {})
-      vim.keymap.set('n', '<C-n>', '<Cmd>BufferLineCyclePrev<CR>', {})
-    end
-  },
 
   -- purescript 2024-03-07 13:36:46 +0800
   -- Syntax highlighting
