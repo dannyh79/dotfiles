784c2d72b1a564bef50da9056bd0b8ebd1bb850b
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 13:46:35 +0800

    Move none-ls config to plugins/

---
+++ lua/plugins/none-ls.lua
@@ -0,0 +1,51 @@
+return {
+  "nvimtools/none-ls.nvim",   -- Use Neovim as a language server to inject LSP diagnostics, code actions, and more via Lua
+  config = function()
+    local null_ls = require("null-ls")
+    local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+
+    local lsp_formatting = function(bufnr)
+      vim.lsp.buf.format({
+        filter = function(client)
+          return client.name == "null-ls"
+        end,
+        bufnr = bufnr,
+      })
+    end
+
+    null_ls.setup {
+      sources = {
+        null_ls.builtins.formatting.prettierd,
+        -- null_ls.builtins.diagnostics.eslint.with({
+        --   diagnostics_format = '[eslint] #{m}\n(#{c})',
+        --   condition = function(utils)
+        --     return utils.root_has_file({ ".eslintrc.js", ".eslintrc.json", ".eslintrc.cjs" })
+        --   end,
+        -- }),
+        -- null_ls.builtins.diagnostics.bash,
+        null_ls.builtins.diagnostics.credo,
+        null_ls.builtins.formatting.mix,
+      },
+      on_attach = function(client, bufnr)
+        if client.supports_method("textDocument/formatting") then
+          vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+          vim.api.nvim_create_autocmd("BufWritePre", {
+            group = augroup,
+            buffer = bufnr,
+            callback = function()
+              lsp_formatting(bufnr)
+            end,
+          })
+        end
+      end
+    }
+
+    vim.api.nvim_create_user_command(
+      'DisableLspFormatting',
+      function()
+        vim.api.nvim_clear_autocmds({ group = augroup, buffer = 0 })
+      end,
+      { nargs = 0 }
+    )
+  end
+}
