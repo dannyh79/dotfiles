72179966af933d8abdae802118b01e7ec9fcf47c
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 13:40:30 +0800

    Move Comment config to plugins/

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -604,39 +604,6 @@ return {
     end
   },
   "windwp/nvim-ts-autotag",
-  {
-    "numToStr/Comment.nvim",
-    opts = {
-      -- add any options here
-    },
-    lazy = false,
-    config = function()
-      require("Comment").setup {
-        pre_hook = function(ctx)
-          -- Only calculate commentstring for tsx filetypes
-          if vim.bo.filetype == 'typescriptreact' then
-            local U = require('Comment.utils')
-
-            -- Determine whether to use linewise or blockwise commentstring
-            local type = ctx.ctype == U.ctype.linewise and '__default' or '__multiline'
-
-            -- Determine the location where to calculate commentstring from
-            local location = nil
-            if ctx.ctype == U.ctype.blockwise then
-              location = require('ts_context_commentstring.utils').get_cursor_location()
-            elseif ctx.cmotion == U.cmotion.v or ctx.cmotion == U.cmotion.V then
-              location = require('ts_context_commentstring.utils').get_visual_start_location()
-            end
-
-            return require('ts_context_commentstring.internal').calculate_commentstring({
-              key = type,
-              location = location,
-            })
-          end
-        end,
-      }
-    end
-  },
 
   -- purescript 2024-03-07 13:36:46 +0800
   -- Syntax highlighting
