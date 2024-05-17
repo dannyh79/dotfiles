39584050fd1d9b9f7bcd53830416935a1f343854
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 12:05:16 +0800

    Move tint config to plugins.lua

--- after/plugin/tint.rc.lua
+++
@@ -1,7 +0,0 @@
-local status, shade = pcall(require, "tint")
-if (not status) then return end
-
-shade.setup {
-  tint = -50,
-  tint_background_colors = false
-}
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -438,7 +438,15 @@ lazy.setup({
   },
   { "lukas-reineke/indent-blankline.nvim", main = "ibl",       opts = {} },
   "tpope/vim-surround",
-  "levouh/tint.nvim",
+  {
+    "levouh/tint.nvim", -- Dims inactive windows
+    config = function()
+      require("tint").setup {
+        tint = -50,
+        tint_background_colors = false
+      }
+    end
+  },
 
   -- themes
   { "catppuccin/nvim",                  as = "catppuccin" },
