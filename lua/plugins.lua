8290d0b4ed92d54fe567b20e223b0d3b8e3faab4
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 11:41:25 +0800

    Move bufferline config to plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -47,7 +47,44 @@ lazy.setup({
     ft = { "markdown" },
     build = function() vim.fn["mkdp#util#install"]() end,
   },
-  "akinsho/nvim-bufferline.lua",
+  {
+    "akinsho/nvim-bufferline.lua",
+    config = function()
+      require("bufferline").setup({
+        options = {
+          mode = "tabs",
+          separator_style = 'slant',
+          always_show_bufferline = false,
+          show_buffer_close_icons = false,
+          show_close_icon = false,
+          color_icons = true
+        },
+        --  highlights = {
+        --    separator = {
+        --      fg = '#073642',
+        --      bg = '#002b36',
+        --    },
+        --    separator_selected = {
+        --      fg = '#073642',
+        --    },
+        --    background = {
+        --      fg = '#657b83',
+        --      bg = '#002b36'
+        --    },
+        --    buffer_selected = {
+        --      fg = '#fdf6e3',
+        --      bold = true,
+        --    },
+        --    fill = {
+        --      bg = '#073642'
+        --    }
+        --  },
+      })
+
+      vim.keymap.set('n', '<C-m>', '<Cmd>BufferLineCycleNext<CR>', {})
+      vim.keymap.set('n', '<C-n>', '<Cmd>BufferLineCyclePrev<CR>', {})
+    end
+  },
 
   "lewis6991/gitsigns.nvim",
   "dinhhuy258/git.nvim", -- For git blame & browse
