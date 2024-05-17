4f576e96913323e446e38168a28a21d7bd1010e7
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-17 13:27:29 +0800

    Move ollama config to plugins/ollama.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -761,48 +761,6 @@ return {
     end
   },
 
-  {
-    "nomnivore/ollama.nvim",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-    },
-
-    -- All the user commands added by the plugin
-    cmd = { "Ollama", "OllamaModel", "OllamaServe", "OllamaServeStop" },
-
-    keys = {
-      -- Sample keybind for prompt menu. Note that the <c-u> is important for selections to work properly.
-      {
-        "<leader>oo",
-        ":<c-u>lua require('ollama').prompt()<cr>",
-        desc = "ollama prompt",
-        mode = { "n", "v" },
-      },
-
-      -- Sample keybind for direct prompting. Note that the <c-u> is important for selections to work properly.
-      {
-        "<leader>oG",
-        ":<c-u>lua require('ollama').prompt('Generate_Code')<cr>",
-        desc = "ollama Generate Code",
-        mode = { "n", "v" },
-      },
-    },
-
-    ---@type Ollama.Config
-    opts = {
-      -- model = "codellama:7b-code-q4_K_M",
-      -- model = "codellama:13b-code-q4_K_M",
-
-      serve = {
-        on_start = true,
-        command = "ollama",
-        args = { "serve" },
-        stop_command = "pkill",
-        stop_args = { "-SIGTERM", "ollama" },
-      },
-    }
-  },
-
   -- purescript 2024-03-07 13:36:46 +0800
   -- Syntax highlighting
   { "purescript-contrib/purescript-vim" },
