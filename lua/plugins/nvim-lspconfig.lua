08bd3a65e3487673cfdb6730386e8c938aa4d641
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-05-20 16:48:47 +0800

    Remove duplicated luarc config from lspconfig

--- lua/plugins/nvim-lspconfig.lua
+++ lua/plugins/nvim-lspconfig.lua
@@ -75,11 +75,6 @@ local servers = {
     end,
     settings = {
       Lua = {
-        diagnostics = {
-          -- Get the language server to recognize the `vim` global
-          globals = { 'vim' },
-        },
-
         workspace = {
           -- Make the server aware of Neovim runtime files
           library = vim.api.nvim_get_runtime_file("", true),
