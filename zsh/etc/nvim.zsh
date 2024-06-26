38e90578ffc60444ee667cab746271f37b4f50cf
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-06-26 18:14:27 +0800

    [zsh] Support multi-line editing w/ nvim by "^X^E"

--- zsh/etc/nvim.zsh
+++ zsh/etc/nvim.zsh
@@ -3,3 +3,8 @@ export TERM="xterm-256color"
 
 # Set default editor as nvim for Git, etc
 export EDITOR=$(which nvim)
+
+# Use "^X^E" to open multi-line editor with nvim ($EDITOR) in zsh
+autoload -z edit-command-line
+zle -N edit-command-line
+bindkey "^X^E" edit-command-line
