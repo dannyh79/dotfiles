18dcd2225d1f816d1a64cc3f6bb2ef50e1475ab2
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2024-10-15 21:04:43 +0800

    [zsh] Add android mobile dev env vars

---
+++ zsh/etc/mobile_dev.zsh
@@ -0,0 +1,5 @@
+# https://docs.expo.dev/workflow/android-studio-emulator/#install-watchman-and-jdk
+export JAVA_HOME=/Library/Java/JavaVirtualMachines/zulu-17.jdk/Contents/Home
+export ANDROID_HOME=$HOME/Library/Android/sdk
+export PATH=$PATH:$ANDROID_HOME/emulator
+export PATH=$PATH:$ANDROID_HOME/platform-tools
