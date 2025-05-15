3103817394b83a0cdf0e7c929e87b7770be702f9
Cheng-Hsuan "Danny" Han <chenghsuan.han@gmail.com>
2025-05-16 01:00:52 +0800

    [zsh] Merge ~/.fzf.zsh into zsh/etc/fzf.zsh

--- zsh/etc/fzf.zsh
+++ zsh/etc/fzf.zsh
@@ -1,2 +1,13 @@
 # https://github.com/junegunn/fzf
-[ -f ~/.fzf.zsh ] && source ~/.fzf.zsh
+
+# Set Homebrew prefix if not already set
+export HOMEBREW_PREFIX="${HOMEBREW_PREFIX:-$(brew --prefix)}"
+
+# Add fzf binary to PATH if missing
+if [[ ":$PATH:" != *":$HOMEBREW_PREFIX/opt/fzf/bin:"* ]]; then
+  export PATH="${PATH:+${PATH}:}$HOMEBREW_PREFIX/opt/fzf/bin"
+fi
+
+# Enable fzf completion and key bindings
+[ -f "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh" ] && source "$HOMEBREW_PREFIX/opt/fzf/shell/completion.zsh"
+[ -f "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh" ] && source "$HOMEBREW_PREFIX/opt/fzf/shell/key-bindings.zsh"
