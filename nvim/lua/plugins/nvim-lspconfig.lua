159e862567f109b8f450d25e979fbedeef96073d
Danny Han <dannyhan@going.cloud>
2026-01-31 20:46:13 +0800

    [nvim] Remove deprecated flow LSP server

--- nvim/lua/plugins/nvim-lspconfig.lua
+++ nvim/lua/plugins/nvim-lspconfig.lua
@@ -65,7 +65,6 @@ end
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
 local servers = {
-  flow = { capabilities = capabilities, on_attach = on_attach, },
   ts_ls = { capabilities = capabilities, on_attach = on_attach, },
   lua_ls = {
     capabilities = capabilities,
