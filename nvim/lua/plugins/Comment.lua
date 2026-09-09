5e85e1bc3600897397bc2a0f121d39a659ed34b2
Danny Han <dhan@Dannys-Mac-mini-4.local>
2026-09-10 07:32:56 +0800

    refactor(nvim): migrate to celeste_comment.nvim for native TS support

--- nvim/lua/plugins/Comment.lua
+++ nvim/lua/plugins/Comment.lua
@@ -1,18 +1,5 @@
 return {
-  "numToStr/Comment.nvim",
-  dependencies = {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-  },
-  opts = {
-    -- add any options here
-  },
+  "celeste3z/celeste_comment.nvim",
   lazy = false,
-  config = function()
-    local ts_hook = require('ts_context_commentstring.integrations.comment_nvim').create_pre_hook()
-    require("Comment").setup {
-      pre_hook = function(ctx)
-        return ts_hook(ctx) or vim.bo.commentstring
-      end,
-    }
-  end
+  opts = {}
 }
